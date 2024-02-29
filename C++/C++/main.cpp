7a211367efc88192b7a952b6414206d9a5f0bd8b
lishangheng <394918428@qq.com>
2024-02-29 13:39:43 +0800

    get_line example

--- C++/C++/main.cpp
+++ C++/C++/main.cpp
@@ -10,9 +10,27 @@
 #include <iomanip>
 using namespace std;
 
-const int NUM_LENS = 4;
+//const int NUM_LENS = 4;
 const int NUM_COLUMNS = 3;
 const int COLUMN_WIDTH = 20;
+
+void PrintOpenFileErr() {
+    cerr << "Can't open file" << endl;
+}
+
+
+void PrintWorldCaptial() {
+    ifstream input("/Users/qt/Documents/C++/C++/C++/C++/world-capitals.txt");
+    if(!input.is_open()) {
+        PrintOpenFileErr();
+    }
+    string capitalName;
+    string countryName;
+    while(getline(input, capitalName) && getline(input, countryName)) {
+        cout << capitalName << "is the capital of " << countryName << endl;
+    }
+        
+}
 
 
 void PrintTableHeader() {
@@ -27,25 +45,43 @@ void PrintTableHeader() {
 void  PrintTableBody() {
     ifstream tableFile("/Users/qt/Documents/C++/C++/C++/C++/table-data.txt");
     if (!tableFile.is_open()) {
-        cerr << "Can't open table-data" << endl;
+        cerr << "Can't open file" << endl;
     }
     
-//    ofstream outputFile("table-data-output.txt");
-    for (int i = 0; i < NUM_LENS; ++i) {
-        int intValue;
-        double doubleValue;
-        tableFile >> intValue >> doubleValue;
-        cout << setw(COLUMN_WIDTH) << i+1 << " | ";
+    int rowNumber = 0;
+    int intValue;
+    double doubleValue;
+
+    while(tableFile >> intValue >> doubleValue) {
+        cout << setw(COLUMN_WIDTH) << rowNumber+1 << " | ";
         cout << setw(COLUMN_WIDTH) << intValue << " | ";
         cout << setw(COLUMN_WIDTH) << doubleValue << endl;
     }
 }
 
+void TestGetLine() {
+//    int dummyInt;
+    string dummyString;
+//    cin >> dummyInt;
+    getline(cin, dummyString);
+    cout << dummyString << endl;
+}
+
+void CinErr() {
+    int myInteger;
+    cin >> myInteger;
+    if(cin.fail()) {
+        cout << "integer get fail:" << boolalpha << cin.fail() << endl;
+    }
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    
-    PrintTableHeader();
-    PrintTableBody();
+    PrintWorldCaptial();
+//    TestGetLine();
+//    PrintTableHeader();
+//    PrintTableBody();
+//    CinErr();
     return 0;
 }
 
