913df6e6b856e588cb8fe3b9622b433987330053
lishangheng <394918428@qq.com>
2024-03-19 17:26:47 +0800

    feat:chapter5

---
+++ C++/C++/helper.cpp
@@ -0,0 +1,8 @@
+//
+//  helper.cpp
+//  C++
+//
+//  Created by qt on 2024/3/19.
+//
+
+#include "helper.hpp"
---
+++ C++/C++/helper.hpp
@@ -0,0 +1,13 @@
+//
+//  helper.hpp
+//  C++
+//
+//  Created by qt on 2024/3/19.
+//
+
+#ifndef helper_hpp
+#define helper_hpp
+
+#include <stdio.h>
+
+#endif /* helper_hpp */
